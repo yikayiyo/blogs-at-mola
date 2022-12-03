b2d3ad22e7294b376e3221125557359b0196c2f8
gaozhiyuan <gaozhiyuan@molaware.com>
2022-12-03 23:53:41 +0800

    feat: init blog

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,30 @@
+export default {
+  title: 'molaware blogs',
+  description: '开心学习。',
+  footer: {
+    message: 'Released under the MIT License.',
+    copyright: 'Copyright © 2022-present molaware'
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      }
+    ],
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      }
+    ]
+  }
+}
